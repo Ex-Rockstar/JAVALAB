1f78ed1380690815d1447bb1b5068cb3d32d5104
Ruthika <ruthirockstar2218@gmail.com>
2023-06-27 20:11:45 +0530

    Print n digits

---
+++ PrintNDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PrintNDigits{
+
+ public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("ENter n");
+	int n=sc.nextInt();
+	for (int i=0;i<=n;i++){
+
+	System.out.println(i+" ");
+}
+
+}
+}
